55f4870936692e7af9158cfb80be24de37e8c970
DayanMonteiro <dmsmamb@gmail.com>
2023-01-30 15:07:34 -0300

    creation of cartItem component layout and remove product add to cart function

--- src/app/CartSlice.js
+++ src/app/CartSlice.js
@@ -1,30 +1,69 @@
 import { createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
 
 const initialState = {
   cartState: false,
-  cartItems: [],
+  cartItems: localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart"))
+    : [],
 };
 
 const CartSlice = createSlice({
   initialState,
   name: "cart",
   reducers: {
+    // altera o navbar para true para abrir o modal
     setOpenCart: (state, action) => {
       state.cartState = action.payload.cartState;
     },
+    // altera o Cart para false pra fechar o modal
     setCloseCart: (state, action) => {
       state.cartState = action.payload.cartState;
     },
-    // ao clicar no botão que recebe esse evento ele vai fazer o push pra add o produto ao array de produtos
+    // add produto no carrinho e soma a quartidade add de um mesmo produto
     setAddItemToCart: (state, action) => {
-      state.cartItems.push(action.payload);
+      const itemIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemIndex >= 0) {
+        state.cartItems[itemIndex].cartQuantity += 1;
+
+        // alert gerado pela lib toast
+        toast.success(`Item Quantity Increased`);
+      } else {
+        const temp = { ...action.payload, cartQuantity: 1 };
+        state.cartItems.push(temp);
+
+        // alert gerado pela lib toast
+        toast.success(`${action.payload.title} added to Cart`);
+      }
+
+      // persistir ps dados dos produtos no carrinho no localStorage
+      localStorage.setItem("cart", JSON.stringify(state.cartItems));
+    },
+
+    setRemoveItemFromCart: (state, action) => {
+      const removeItem = state.cartItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+
+      state.cartItems = removeItem;
+      localStorage.setItem("cart", JSON.stringify(state.cartItems));
+
+      toast.success(`${action.payload.title} Removed From Cart`);
     },
   },
 });
 
-export const { setOpenCart, setCloseCart, setAddItemToCart } =
-  CartSlice.actions;
+export const {
+  setOpenCart,
+  setCloseCart,
+  setAddItemToCart,
+  setRemoveItemFromCart,
+} = CartSlice.actions;
 
 export const selectCartState = (state) => state.cart.cartState;
+export const selectCartItems = (state) => state.cart.cartItems;
 
 export default CartSlice.reducer;
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectCartState, setCloseCart } from "../app/CartSlice";
+import {
+  selectCartItems,
+  selectCartState,
+  setCloseCart,
+} from "../app/CartSlice";
 import CartCount from "./cart/CartCount";
 import CartEmpty from "./cart/CartEmpty";
 import CartItem from "./cart/CartItem";
@@ -8,6 +12,9 @@ import CartItem from "./cart/CartItem";
 const Cart = () => {
   const dispatch = useDispatch();
   const ifCartState = useSelector(selectCartState);
+  const cartItem = useSelector(selectCartItems);
+
+  console.log("cartItemn", cartItem);
 
   const onCartToggle = () => {
     dispatch(
@@ -31,8 +38,40 @@ const Cart = () => {
           blur-effect-theme h-screen max-w-xl w-full absolute right-0 `}
         >
           <CartCount onCartToggle={onCartToggle} />
-          <CartEmpty />
-          <CartItem />
+
+          {cartItem?.length === 0 ? (
+            <CartEmpty />
+          ) : (
+            <div>
+              <div className="flex items-start justify-start flex-col gap-y-7 lg:gap-y-5 overflow-y-scroll h-[81vh] scroll-smooth scroll-hidden py-3">
+                {cartItem?.map((item, i) => (
+                  <CartItem key={i} item={item} />
+                ))}
+              </div>
+
+              <div className="fixed bottom-0 bg-white w-full px-5 py-2 grid items-center">
+                <div className="flex items-center justify-between">
+                  <h1 className="text-base font-semibold uppercase">
+                    SubTotal
+                  </h1>
+                  <h1 className="text-sm rounded bg-theme-cart text-slate-100 px-1 py-0.5">
+                    {/* ${totalAmount} */} 000
+                  </h1>
+                </div>
+                <div className="grid items-center gap-2">
+                  <p className="text-sm font-medium text-center">
+                    Taxes and Shipping Will Calculate At Shipping
+                  </p>
+                  <button
+                    type="button"
+                    className="button-theme bg-theme-cart text-white"
+                  >
+                    Check Out
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
